042202b1c89388ad1b1699c99c5ae3cd878b06a9
박정민 <163364733+nicejmp1@users.noreply.github.com>
2024-06-11 15:44:19 +0900

    2024

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
-        Header
-    </div>
+    <header id="header" role="banner">
+        <div class="header__inner">
+            <div class="header__logo"></div>
+            <nav class="header__nav" role="navigation"></nav>
+            <div class="header__nav__mobile"></div>
+        </div>
+    </header>
 </template>
